8304036f7a34c6a261340e061ea0b96c59712a61
Nathan Patnaude <nathanpatnaude@gmail.com>
2022-12-10 17:14:07 -0500

    added text for the readme markdown, put license object into the rendered license fucntions

--- index.js
+++ index.js
@@ -1,75 +1,74 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-
+const generateMarkdown = require("./utils/generateMarkdown")
+const Licensesobj = require('./utils/Licenseobj');
+const Licenses = Licensesobj.getLicenseNames();
 // TODO: Create an array of questions for user input
-const questions = [ {
+const questions = [{
     type: 'input',
     name: 'title',
     message: 'Please enter the project title:',
-  },
-  {
+},
+{
     type: 'input',
     name: 'description',
     message: 'Enter the project description:',
-  },
-  {
+},
+{
     type: 'input',
     name: 'installation',
     message: 'Enter installation instructions:',
-  },
-  {
+},
+{
     type: 'input',
     name: 'usage',
     message: 'Enter the projects usage information:',
-  },
-  {
+},
+{
     type: 'input',
     name: 'contributing',
     message: 'Enter contributers to this project:',
-  },
-  {
+},
+{
     type: 'input',
     name: 'tests',
     message: 'Enter tests included:',
-  },
-  {
+},
+{
     type: "list",
     name: "license",
     message: "Choose the appropriate license used for this project:",
-    choices: [
-        "None",
-        "Apache License 2.0",
-        "GNU General Public License v3.0",
-        "MIT License",
-        "BSD 2-Clause Simplified License",
-        "BSD 3-Clause New or Revised License",
-        "Boost Software License 1.0",
-        "Creative Commons Zero v1.0 Universal",
-        "Eclipse Public License 2.0",
-        "GNU Affero General Public License 3.0",
-        "GNU General Public License v2.0",
-        "GNU Lesser General Public License v2.1",
-        "Mozilla Public License 2.0",
-        "Unlicense"
-    ]
+    choices: Licenses
 },
-  {
+{
     type: 'input',
     name: 'githubUser',
     message: 'Enter your Github username:',
-  },
-  {
+},
+{
     type: 'input',
     name: 'email',
     message: 'Enter your Email address:',
-  },];
+},];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile('./' + fileName, data, (err) =>
+        err ? console.log(err) : console.log('Successfully created ' + fileName + '!')
+    );
+
+}
 
 // TODO: Create a function to initialize app
 function init() {
+    inquirer.prompt(questions)
+        .then((answers) => {
+            const content = generateMarkdown(answers);
+            writeToFile('README.md', content);
+
+        });
+
 
 }
 
---
+++ utils/Licenseobj.js
@@ -0,0 +1,99 @@
+const LicenseOptions = [{
+    name: "None",
+    abrv: "none",
+    link: "none"
+},
+{
+    name: "Apache License 2.0",
+    abrv: "apache-2.0",
+    link: "https://choosealicense.com/licenses/apache-2.0/"
+},
+{
+    name: "GNU General Public License v3.0",
+    abrv: "lgpl-3.0",
+    link: "https://choosealicense.com/licenses/lgpl-3.0/"
+},
+{
+    name: "MIT License",
+    abrv: "mit",
+    link: "https://choosealicense.com/licenses/mit/"
+},
+{
+    name: "BSD 2-Clause “Simplified” License",
+    abrv: "bsd-2-clause",
+    link: "https://choosealicense.com/licenses/bsd-2-clause/"
+},
+{
+    name: "BSD 3-Clause New or Revised License",
+    abrv: "bsd-3-clause",
+    link: "https://choosealicense.com/licenses/bsd-3-clause/"
+},
+{
+    name: "Boost Software License 1.0",
+    abrv: "bsl-1.0",
+    link: "https://choosealicense.com/licenses/bsl-1.0/"
+},
+{
+    name: "Creative Commons Zero v1.0 Universal",
+    abrv: "cc0-1.0",
+    link: "https://choosealicense.com/licenses/cc0-1.0/"
+},
+{
+    name: "Eclipse Public License 2.0",
+    abrv: "epl-2.0",
+    link: "https://choosealicense.com/licenses/epl-2.0/"
+},
+{
+    name: "GNU Affero General Public License 3.0",
+    abrv: "agpl-3.0",
+    link: "https://choosealicense.com/licenses/agpl-3.0/"
+},
+{
+    name: "GNU General Public License v2.0",
+    abrv: "gpl-2.0",
+    link: "https://choosealicense.com/licenses/gpl-2.0"
+},
+{
+    name: "GNU Lesser General Public License v2.1” License",
+    abrv: "gpl-3.0",
+    link: "https://choosealicense.com/licenses/gpl-3.0"
+},
+{
+    name: "Mozilla Public License 2.0",
+    abrv: "mpl-2.0",
+    link: "https://choosealicense.com/licenses/mpl-2.0"
+},
+{
+    name: "The Unlicense",
+    abrv: "unlicense",
+    link: "https://choosealicense.com/licenses/unlicense"
+},
+];
+
+const getLicenseNames = () => {
+    return (LicenseOptions.map(a => a.name));
+};
+
+function getLicenseAbrv(choice) {
+    for (let i = 0; i < LicenseOptions.length; i++) {
+        if (choice === LicenseOptions[i].name) {
+            return LicenseOptions[i].abrv;
+        };
+    };
+};
+
+function getLicenseLink(choice) {
+    for (let i = 0; i < LicenseOptions.length; i++) {
+        if (choice === LicenseOptions[i].name) {
+            return LicenseOptions[i].link;
+        };
+    };
+};
+
+module.exports = {
+LicenseOptions,
+getLicenseAbrv,
+getLicenseLink,
+getLicenseNames,
+
+}
